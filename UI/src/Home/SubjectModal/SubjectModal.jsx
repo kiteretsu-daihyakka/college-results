e2b67e0cd6d656e8c187326215aa2feb9b246fe4
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-03-26 09:37:40 +0530

    Update SubjectModal.jsx

--- UI/src/Home/SubjectModal/SubjectModal.jsx
+++ UI/src/Home/SubjectModal/SubjectModal.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react"
-import Modal from "../../Web/Modal"
+import { useState } from "react";
+import Modal from "../../Web/Modal";
+import "./SubjectModal.css";
 
 const SubjectModal = (props) => {
-    // const [modalOpen, setModalOpen] = useState();
+  const semDetail = props.allSem.filter((sem) => sem.id === props.semID)[0];
 
-    return (<Modal open={props.show}>
-        <div>Header</div>
-        <div>Body</div>
-        <div>
-            <button onClick={()=>{props.onClose()}}>Close</button>
+  return (
+    <Modal open={props.show}>
+      <div className="modal-header">
+        <div className="modal-title">Subjects</div>
+        <div className="modal-subtitle">
+          <p>{semDetail?.name}</p>
         </div>
-    </Modal>)
-}
-export default SubjectModal
+      </div>
+      <div className="modal-body">
+        <table>
+          <tbody>
+            {semDetail?.subjects.map((subj) => (
+              <tr>
+                <td>{subj.name}</td>
+                <td>&gt;</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            props.onClose();
+          }}
+        >
+          Close
+        </button>
+      </div>
+    </Modal>
+  );
+};
+export default SubjectModal;
