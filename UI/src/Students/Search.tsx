2f71a88e9a47ec0f9bf7bafc22e9353032098bdf
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 22:08:58 +0530

    Create Search.tsx

---
+++ UI/src/Students/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SearchPNG from "../images/PNG/Vectorsearch.png";
+
+export const Search = () => {
+  return (
+    <div className="searchStudent">
+        <img src={SearchPNG} alt="search icon" />
+        <input type="text" placeholder="Search student name or email" />
+      </div>
+  )
+}
