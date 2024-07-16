5407ccc04ac56485016453c761510ddac6ad2050
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 21:57:51 +0530

    Create SemesterProvider.tsx

---
+++ UI/src/Home/SemesterCards/SemesterProvider.tsx
@@ -0,0 +1,21 @@
+// import { useContext, createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { domain } from "../../Utils/Variables.js";
+
+// const semContext = createContext();
+let semesterURL = domain.concat("semester");
+
+// const SemesterProvider = ({children}) =>{
+    
+//     return <semContext.Provider value={{ fetchSemesters }}>
+//             {children}
+//         </semContext.Provider>;
+// }
+async function fetchSemesters () {
+    const response = await axios(semesterURL);
+    return response.data.data;
+}
+// export default SemesterProvider;
+export default fetchSemesters;
+
+// export const useSemesters = () => useContext(semContext);
