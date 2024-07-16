21bd30b6930975229789ed0bc1cd5f0abe12b9e6
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 22:07:51 +0530

    Create Actions.tsx

---
+++ UI/src/Students/Actions.tsx
@@ -0,0 +1,198 @@
+import React, { useRef } from "react";
+import { useState } from "react";
+import Button from "../Web/Button";
+import { AddStudent } from "./AddStudent.tsx";
+import { EditStudent } from "./EditStudent.tsx";
+import { Delete } from "./Delete.tsx";
+import { ChangeSem } from "./ChangeSem.tsx";
+
+import DeleteNoShadow from "../images/PNG/delete-no-shadow.png";
+
+import DeleteDark from "../images/PNG/delete-dark.png";
+import ExchangeDark from "../images/PNG/exchange-dark.png";
+import AddStudentDark from "../images/PNG/add-student-dark.png";
+
+import AddStudentWhite from "../images/PNG/add-student-white.png";
+import DeleteWhite from "../images/PNG/delete-white.png";
+import ExchangeWhite from "../images/PNG/exchange-white.png";
+
+import EditPNG from "../images/PNG/EditPNG.png";
+import { useAuth } from "../hooks/AuthProvider";
+import { StudentForm } from "./StudentForm.tsx";
+
+export const Actions = (props) => {
+  const auth = useAuth();
+
+  const DeleteImgRef = useRef(null);
+  const ExchangeImgRef = useRef(null);
+  const StudentAddImgRef = useRef(null);
+
+  const [showDeleteModal, setshowDeleteModal] = useState(false);
+  const [showChangeSemModal, setshowChangeSemModal] = useState(false);
+  const [studentDetailEdit, setstudentDetailEdit] = useState(null);
+  const [showAddStudentModal, setshowAddStudentModal] = useState(false);
+  const [showEditStudentModal, setshowEditStudentModal] = useState(false);
+  const {
+    studentsData,
+    IDs_studentSelected,
+    current_semID,
+    semesters,
+    fetchSemesterWiseStudent,
+    setIDs_studentSelected,
+    singleStudentDelete,
+  } = props;
+
+  const showAddModal = () => {
+    setshowAddStudentModal(true);
+  }
+  const hideAddModal = () => {
+    setshowAddStudentModal(false);
+  }
+  const showEditModal = (student) => {
+    setshowEditStudentModal(true);
+    setstudentDetailEdit(student);
+  }
+  const hideEditModal = () => {
+    setshowEditStudentModal(false);
+  }
+  // function handleMouseEnter(e) {
+  //   if (e.target.id === "delete") {
+  //     DeleteImgRef.current.src = DeleteWhite;
+  //   } else if (e.target.id === "exchange") {
+  //     ExchangeImgRef.current.src = ExchangeWhite;
+  //   } else {
+  //     StudentAddImgRef.current.src = AddStudentWhite;
+  //   }
+  // }
+  // function handleMouseLeave(e) {
+  //   if (e.target.id === "delete") {
+  //     DeleteImgRef.current.src = DeleteDark;
+  //   } else if (e.target.id === "exchange") {
+  //     ExchangeImgRef.current.src = ExchangeDark;
+  //   } else {
+  //     StudentAddImgRef.current.src = AddStudentDark;
+  //   }
+  // }
+
+  return (
+    <>
+      <div
+        className="studentActions"
+        // style={{ visibility: studentsData !== null ? "visible" : "hidden" }}
+      >
+        {auth.teacher && studentsData !== null && (
+          <>
+            {IDs_studentSelected.length > 0 && (
+              <>
+                <Button
+                  className="studentActionBtn"
+                  id="exchange"
+                  onMouseOver={() => ExchangeImgRef.current.src = ExchangeWhite}
+                  onMouseOut={() => ExchangeImgRef.current.src = ExchangeDark}
+                  onClick={() => setshowChangeSemModal(true)}
+                >
+                  <img src={ExchangeDark} alt="Change Semester" 
+                  ref={ExchangeImgRef}
+                  />
+                  <span>Change Semester</span>
+                </Button>
+                <Button
+                  className="studentActionBtn"
+                  id="delete"
+                  onMouseOver={() => DeleteImgRef.current.src = DeleteWhite}
+                  onMouseOut={() => DeleteImgRef.current.src = DeleteDark}
+                  onClick={() => setshowDeleteModal(true)}
+                >
+                  <img src={DeleteDark} alt="Delete Selected Student(s)" ref={DeleteImgRef}/>
+                  <span>Delete</span>
+                </Button>
+              </>
+            )}
+            <Button
+              className="studentActionBtn primary"
+              id="add-student"
+              onMouseOver={() => StudentAddImgRef.current.src = AddStudentDark}
+              onMouseOut={() => StudentAddImgRef.current.src = AddStudentWhite}
+              onClick={showAddModal}
+            >
+              <img src={AddStudentWhite} alt="Add Student" ref={StudentAddImgRef}/>
+              <span>Add Student</span>
+            </Button>
+          </>
+        )}
+      </div>
+      <>
+      {(showAddStudentModal || showEditStudentModal) && 
+        <StudentForm
+          mode={showAddStudentModal ? "Add" : "Edit"}
+          showModal={props.showModal}
+          hideModal={props.hideModal}
+          current_semID={props.current_semID}
+          semesters={props.semesters}
+          refresh={props.refresh}
+          studentDetail={
+            showAddStudentModal
+              ? { id: "", name: "", email: "" }
+              : props.studentDetail
+          }
+        />
+        }
+        {showDeleteModal && (
+          <Delete
+            onSuccessDelete={() => {
+              setIDs_studentSelected([]);
+              setshowDeleteModal(false);
+              fetchSemesterWiseStudent(current_semID);
+            }}
+            onClose={() => {
+              if (singleStudentDelete) {
+                setIDs_studentSelected([]);
+              }
+              // TODO: single student delete pending
+              //   singleStudentDelete = false;
+              setshowDeleteModal(false);
+            }}
+            ids={IDs_studentSelected}
+          />
+        )}
+        {showChangeSemModal && (
+          <ChangeSem
+            semesters={semesters}
+            onSuccessChange={() => {
+              setIDs_studentSelected([]);
+              setshowChangeSemModal(false);
+            }}
+            refresh={() => fetchSemesterWiseStudent(current_semID)}
+            onClose={() => setshowChangeSemModal(false)}
+            studentIDs={IDs_studentSelected}
+            current_semID={current_semID}
+          />
+        )}
+      </>
+    </>
+  );
+};
+const SingleStudentActions = (props) => {
+  const { student, showEditModal } = props;
+
+  return (
+    <>
+      <img
+        src={EditPNG}
+        alt="Edit Student"
+        id="edit-student"
+        className="studentActionBtn studentActionTable"
+        onClick={() => showEditModal(student)}
+      />
+      <img
+        src={DeleteNoShadow}
+        alt="Edit Student"
+        id="delete-student"
+        className="studentActionBtn studentActionTable deleteIcon"
+        onClick={() => setshowDeleteModal(true)}
+      />
+    </>
+  );
+};
+
+export { SingleStudentActions };
