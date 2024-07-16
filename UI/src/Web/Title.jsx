a3801c95b2eb14a7a6fe9c464efbd059f0a87ca5
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 22:29:41 +0530

    Update Title.jsx

--- UI/src/Web/Title.jsx
+++ UI/src/Web/Title.jsx
@@ -2,7 +2,7 @@ import {Helmet} from 'react-helmet'
 
 const Title = (props) => {
     return <Helmet>
-        <title>{props.children}</title>
+        <title>{props.children} | Just College</title>
     </Helmet>
 }
 export default Title
