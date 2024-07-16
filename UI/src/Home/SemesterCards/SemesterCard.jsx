540349450579ebef87ea47c08986dc606a69972d
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 21:59:53 +0530

    Update SemesterCard.jsx

--- UI/src/Home/SemesterCards/SemesterCard.jsx
+++ UI/src/Home/SemesterCards/SemesterCard.jsx
@@ -1,36 +1,76 @@
+import Loading, {BoxLoading, SingleLineLoading} from "./Loading.tsx";
 import "./SemesterCard.css";
+import VerticalLine from "./VerticalLine";
+import { useNavigate } from "react-router-dom";
+
 
 function SemesterCard(props) {
-  return (
-    <tr className="sem-card">
-      <td key={`${props.semData.name}-name`} className="sem-name">
-        {props.semData.name}
-      </td>
-      <td key={`${props.semData.name}-subjects`} className="subCell">
-        Subjects{" "}
-        <span className="countLinkCell">
-          <button
-            type="button"
-            className="btn"
-            onClick={() => {
-              props.linkClickModal(props.semData['id']);
-            }}
-          >
-            {props.semData.subjects.length}
-          </button>
-        </span>
-        {/* <br /> */}
-      </td>
-      <td key={`${props.semData.name}-students`} className="studCell">
-        Students{" "}
-        <span className="countLinkCell">{props.semData.studCount}</span>
-        {/* <br /> */}
-      </td>
-      <td key={`${props.semData.name}-results`} className="viewResultCell">
-        View Results
-        {/* <br /> */}
-      </td>
-    </tr>
+  const navi = useNavigate();
+  
+  const modalOpenHandler = () =>{
+    if (subjects?.length !== 0) {
+      props.linkClickModal(props.semData["id"]);
+    }
+  }
+  const studentLinkClicked = () => {
+    navi(`student/semester/${props.semData["id"]}`);
+  }
+  let name = null;
+  let subjects = null;
+  let studCount = null;
+
+  if (props.semData) {
+    name = props.semData.name;
+    subjects = props.semData.subjects;
+    studCount = props.semData.studCount;
+  }
+ 
+  let card = (
+    <div className={`sem-card ${props.loading ? "text-loader" :""}`}>
+      
+      {props.loading ? 
+        <>
+        <Loading/>
+        {!props.isMobileScreen && 
+        <>
+          <BoxLoading/>
+          <BoxLoading/>
+          <BoxLoading/>
+        </>}
+       </>:
+        <>
+        <div key={`${name}-name`} className={`sem-name ${props.loading ? "loadingSquare":""}`}>
+      {name}
+      </div>
+      <div key={`${name}-subjects`} className={`subCell`}>
+      
+        <div>Subjects</div>
+        <div
+          className={`countLinkCell underlined ${
+            subjects?.length === 0 ? "inactive" : ""
+          }`}
+          onClick={modalOpenHandler}
+        >
+          {subjects?.length}
+        </div>
+
+      </div>
+
+      <VerticalLine />
+ 
+      <div key={`${name}-students`} className={`studCell`}>
+      <div>Students</div>
+        <div className="countLinkCell underlined" onClick={studentLinkClicked}>
+          {studCount}
+        </div>
+      </div>
+      <VerticalLine />
+      <div key={`${name}-results`} className="viewResultCell">
+        View<br />Results
+      </div>
+        </>}
+    </div>
   );
+  return card;
 }
 export default SemesterCard;
