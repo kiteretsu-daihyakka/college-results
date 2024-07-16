64d5b8bc0ffa60bde066ad85231c80cc10fbfaa7
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 22:28:55 +0530

    Update Modal.jsx

--- UI/src/Web/Modal.jsx
+++ UI/src/Web/Modal.jsx
@@ -1,8 +1,23 @@
-import "./Modal.css"
+import Backdrop from "./Backdrop";
+import "./Modal.css";
 
 const Modal = (props) => {
-    return <dialog open={props.open} className="dialog-modal">
-        {props.children}
-    </dialog>
-}
-export default Modal
+  return (
+    <div className={`${props.className}`}>
+      {props.backdropEntire && (
+        <div className="backdrop-entire" onClick={props.onClose}></div>
+      )}
+      <Backdrop
+        className={`backdrop-modal ${props.onBlurClass} ${props.backDropClass}`}
+        onClick={props.onClose}
+      />
+
+      <div className={`dialog-modal-container`}>
+        <dialog open={props.open} className="dialog-modal">
+          {props.children}
+        </dialog>
+      </div>
+    </div>
+  );
+};
+export default Modal;
