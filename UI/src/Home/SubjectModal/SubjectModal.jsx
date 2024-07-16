ffbbad536fe8a9755f63cc2fc4ea62bf4991a340
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-07-16 22:01:32 +0530

    Update SubjectModal.jsx

--- UI/src/Home/SubjectModal/SubjectModal.jsx
+++ UI/src/Home/SubjectModal/SubjectModal.jsx
@@ -1,40 +1,40 @@
 import { useState } from "react";
 import Modal from "../../Web/Modal";
 import "./SubjectModal.css";
+// import RightArrow from "../../images/right_arrow.png";
 
 const SubjectModal = (props) => {
-  const semDetail = props.allSem.filter((sem) => sem.id === props.semID)[0];
+  const semDetail = Array.prototype.filter.call(props.allSem, (sem) => sem?.id === props.semID)[0];
 
   return (
-    <Modal open={props.show}>
+    <>
+    {props.show && <Modal open={props.show} onClose={props.onClose} className="subjectModal" onBlurClass={` ${props.isMobileScreen ? '' : 'no-blur'}`}>
       <div className="modal-header">
         <div className="modal-title">Subjects</div>
         <div className="modal-subtitle">
-          <p>{semDetail?.name}</p>
+          {semDetail?.name}
         </div>
       </div>
       <div className="modal-body">
-        <table>
+        <table className="subjects-list-modal">
           <tbody>
             {semDetail?.subjects.map((subj) => (
-              <tr>
-                <td>{subj.name}</td>
-                <td>&gt;</td>
+              <tr key={subj.name}>
+                <td className="name">{subj.name}</td>
+                {/* <td>
+                  <img src={RightArrow} />
+                </td> */}
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <div>
-        <button
-          onClick={() => {
-            props.onClose();
-          }}
-        >
+      <div className="modal-footer">
+        <button onClick={props.onClose}>
           Close
         </button>
       </div>
-    </Modal>
+    </Modal>}</>
   );
 };
 export default SubjectModal;
