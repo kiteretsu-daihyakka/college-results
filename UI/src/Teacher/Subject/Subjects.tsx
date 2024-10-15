a132cc613ec607a5e642bff715c0a6a852849f92
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-10-16 00:34:44 +0530

    Update Subjects.tsx

--- UI/src/Teacher/Subject/Subjects.tsx
+++ UI/src/Teacher/Subject/Subjects.tsx
@@ -1,24 +1,30 @@
-import { useState } from "react";
-import Semester from "../Semester"
-import Subject from "./Subject"
-import Add from "./Add"
+import { useEffect, useState } from "react";
+import Semester from "../Semester";
+import Subject from "./Subject";
+import Add from "./Add";
 import Title from "../../Web/Title";
+import fetchSemesters from "../../Home/SemesterCards/SemesterProvider.tsx";
 
 const Subjects = (props) => {
-    const [subs, setSubs] = useState([]);
-    function onSemesterChange(id){
-        if (id === "")
-            return;
-        setSubs(props.allSemOverview.filter(sem => parseInt(sem.id) === parseInt(id))[0].subjects)
-    }
-    return (<>
-    <Title>Subjects</Title>
-        <br />
-        <Semester onChange={onSemesterChange} allSemOverview={props.allSemOverview}/>
-        <br />
-        <h3>Subjects:</h3>
-        {subs.map(sub => <Subject key={sub.name} name={sub.name}/>)}
-        {/* <Add/> */}
-    </>)
-} 
-export default Subjects
+  const [subs, setSubs] = useState([]);
+//   const [semesters, setSemesters] = useState([null, null, null, null]);
+  
+  useEffect(() => {
+    // fetchSemesters().then((semesters) => {
+    //   setSemesters(semesters);
+    // });
+  }, []);
+  return (
+    <>
+      <Title>Subjects</Title>
+      
+      <br />
+      <h3>Subjects:</h3>
+      {subs.map((sub) => (
+        <Subject key={sub.name} name={sub.name} />
+      ))}
+      {/* <Add/> */}
+    </>
+  );
+};
+export default Subjects;
