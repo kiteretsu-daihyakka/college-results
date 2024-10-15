87f3bbc38b771f21f4b21405f95442b6c438c0a5
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2024-10-16 00:31:24 +0530

    Update Delete.tsx

--- UI/src/Students/Delete.tsx
+++ UI/src/Students/Delete.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
 import Button from "../Web/Button.jsx";
 import { domain } from "../Utils/Variables.js";
-import Modal from '../Web/Modal.jsx';
-import Toast from '../Web/Toast.jsx';
+import Modal from "../Web/Modal.jsx";
+import Toast from "../Web/Toast.jsx";
 
 export const Delete = (props) => {
   const studentDomain = "student";
-  const deleteStudents = async () => {
-    let data = {
-      "students": props.ids
+  let data;
+  if (props.singleStudentDelete) {
+    data = {
+      students: [props.studentDetail["id"]],
     };
-    // console.log(data);
+  } else {
+    data = {
+      students: props.ids,
+    };
+  }
+  let selectedStudents = props.studentsData.filter((student) =>
+    data.students.includes(student.id)
+  );
+  console.log(data, selectedStudents);
+  const deleteStudents = async () => {
     let config = {
       method: "delete",
       url: domain.concat(studentDomain),
@@ -22,39 +32,53 @@ export const Delete = (props) => {
       },
       data: data,
     };
-    console.log(props.ids);
+
     // return;
     axios(config)
       .then((response) => {
-        <Toast>Student(s) Deleted Successfully.</Toast>
+        <Toast>Student(s) Deleted Successfully.</Toast>;
+        props.setIDs_studentSelected((prev) => {
+          return prev.filter((id) => !data.students.includes(id));
+        });
+        props.setStudentsData((prev) => {
+          return prev.filter((student) => !data.students.includes(student.id));
+        });
         props.onSuccessDelete();
       })
       .catch((errors) => {
         console.log(errors);
       });
-  }
+  };
   return (
-      <Modal
-        open={true}
-        onClose={props.onClose}
-        className="student-page-modal"
-        onBlurClass={`${true ? "" : "no-blur"}`}
-        backdropEntire={true}
-      >
-        <form>
-          <div className="modal-header">
-            <div className="modal-title">Delete Student</div>
-          </div>
-          <div className="modal-body addStudentModal">
-            Are you sure you want to delete the selected students?
-          </div>
-          <div className="modal-footer">
-            <Button type="button" onClick={deleteStudents}>Delete</Button>
-            <Button type="button" onClick={props.onClose}>
-              Cancel
-            </Button>
-          </div>
-        </form>
-      </Modal>
-    )
-}
+    <Modal
+      open={true}
+      onClose={props.onClose}
+      className="deleteModal student-page-modal"
+      onBlurClass={`${true ? "" : "no-blur"}`}
+      backdropEntire={true}
+    >
+      <form>
+        <div className="modal-header ">
+          <div className="modal-title">Delete Student</div>
+        </div>
+        <div className="modal-body">
+          Are you sure you want to delete the following {selectedStudents.length} student(s)?:
+          <br/>
+          {selectedStudents.map((student, index) => (
+            <div style={{ fontWeight: "bold", paddingLeft: "15px" }}>
+              {index+1} . {student.name}
+            </div>
+          ))}
+        </div>
+        <div className="modal-footer">
+          <Button type="button" onClick={deleteStudents}>
+            Delete
+          </Button>
+          <Button type="button" onClick={props.onClose}>
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
