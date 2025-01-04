9122efae6d5c8b87c5137a48928e84b6ea9829b0
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2025-01-04 15:40:55 +0530

    Update SemFilter.tsx

--- UI/src/Students/SemFilter.tsx
+++ UI/src/Students/SemFilter.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import { SingleLineLoading } from "../Home/SemesterCards/Loading.tsx";
+import './styles/SemFilter.css';
 
 export const SemFilter = (props) => {
   const navi = useNavigate();
-  const { semesters, current_semID, fetchSemesterWiseStudent } = props;
+  const params = useParams();
+  const { semesters, action_path } = props;
+
+  let current_semID = parseInt(params["semester"]);
   return (
     <div className="semesterFilter">
       <div className="semesterFilterHeader">Semesters</div>
       {semesters.map((sem, index) => (
         <div
           key={index}
-          className={`SemesterTabFilter ${
+          className={`SemesterTabFilter ${ // this class parent based two definition, horizontal and vertical
             sem?.id === current_semID ? "activeSemesterTabFilter" : ""
           }`}
           onClick={() => {
-            navi(`/student/semester/${sem?.id}`);
-            fetchSemesterWiseStudent(sem?.id);
+            navi(`${action_path}/${sem?.id}`);// given url
           }}
           title={sem == null ? "" : `Select ${sem?.name}`}
         >
           {sem == null ? (
-            <SingleLineLoading className={"fullwidth"} />
+            <SingleLineLoading className={`fullwidth ${'className' in props ? props.className : ''}`} />
           ) : (
             sem?.name
           )}
