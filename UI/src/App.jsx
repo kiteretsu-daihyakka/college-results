b337fd3b04784829d0ce81dde2c170e10ced2140
Sahil Thakor <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2025-01-04 15:38:05 +0530

    Update App.jsx

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,95 +1,111 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
+import {BrowserRouter, Routes, Route, useNavigate, Navigate} from "react-router-dom";
+import {useMediaQuery} from "react-responsive";
 import Header from "./Web/Header.jsx";
 import Result from "./Result/Result.jsx";
 import Home from "./Home/Home.jsx";
 import Subjects from "./Teacher/Subject/Subjects.tsx";
 import TeacherLogin from "./Teacher/TeacherLogin/TeacherLogin.tsx";
 import Students from "./Students/Students.tsx";
+import UpdateMarks from "./Marks/UpdateMarks.tsx";
 
 /* providers */
 import AuthProvider from "./hooks/AuthProvider.jsx";
+import HeaderWrapper from "./Wrappers/HeaderWrapper";
+import ProtectedRoute from "./Web/Security/ProtectedRoute";
+
 
 /* Variables */
 
 function App() {
-  const isMobileScreen = useMediaQuery({
-    query: "only screen and (max-width: 870px)",
-  });
-  var _ = require('lodash');
-  return (
-    <AuthProvider>
-      {/* <SemesterProvider> */}
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Header isMobileScreen={isMobileScreen} />
-                  <section
-                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
-                  >
-                    <Home isMobileScreen={isMobileScreen} />
-                  </section>
-                </>
-              }
-            />
-            <Route
-              path="/results"
-              element={
-                <>
-                  <Header isMobileScreen={isMobileScreen} />
-                  <section
-                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
-                  >
-                    <Result />
-                  </section>
-                </>
-              }
-            />
-            <Route
-              path="/student/semester/:semester"
-              element={
-                <>
-                  <Header isMobileScreen={isMobileScreen} />
-                  <section
-                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
-                  >
-                    <Students />
-                  </section>
-                </>
-              }
-            />
-            <Route
-              path="/subjects"
-              element={
-                <>
-                  <Header isMobileScreen={isMobileScreen} />
-                  <section
-                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
-                  >
-                    <Subjects />
-                  </section>
-                </>
-              }
-            />
-            <Route path="/teacher-login" element={<TeacherLogin />} />
-            <Route
-              path="*"
-              element={
-                <section className={`${isMobileScreen ? "mobileScreen" : ""}`}>
-                  <p>Page not found.</p>
-                </section>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      {/* </SemesterProvider> */}
-    </AuthProvider>
-  );
+    const isMobileScreen = useMediaQuery({
+        query: "only screen and (max-width: 870px)",
+    });
+
+    const _ = require('lodash');
+
+    return (
+        <AuthProvider>
+            {/* <SemesterProvider> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <HeaderWrapper isMobileScreen={isMobileScreen}>
+                                <section
+                                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
+                                >
+                                    <Home isMobileScreen={isMobileScreen}/>
+                                </section>
+                            </HeaderWrapper>
+                        }
+                    />
+                    <Route
+                        path="/students/semester/:semester"
+                        element={
+                            <HeaderWrapper isMobileScreen={isMobileScreen}>
+                                <section
+                                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
+                                >
+                                    <Students/>
+                                </section>
+                            </HeaderWrapper>
+                        }
+                    />
+                    <Route
+                        path="update-marks/semester/:semester"
+                        element={
+                            <ProtectedRoute>
+                                <HeaderWrapper isMobileScreen={isMobileScreen}>
+                                    <section className={`${isMobileScreen ? "mobileScreen" : ""} update-marks`}>
+                                        <UpdateMarks/>
+                                    </section>
+                                </HeaderWrapper>
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/subjects"
+                        element={
+                            <HeaderWrapper isMobileScreen={isMobileScreen}>
+                                <section
+                                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
+                                >
+                                    <Subjects/>
+                                </section>
+                            </HeaderWrapper>
+                        }
+                    />
+                    <Route
+                        path="/results"
+                        element={
+                            <HeaderWrapper isMobileScreen={isMobileScreen}>
+                                <section
+                                    className={`${isMobileScreen ? "mobileScreen" : ""}`}
+                                >
+                                    <Result/>
+                                </section>
+                            </HeaderWrapper>
+                        }
+                    />
+                    <Route
+                        path="*"
+                        element={
+                            <HeaderWrapper isMobileScreen={isMobileScreen}>
+                                <section className={`${isMobileScreen ? "mobileScreen" : ""}`}>
+                                    <p>Page not found.</p>
+                                </section>
+                            </HeaderWrapper>
+                        }
+                    ></Route>
+                    <Route path="/teacher-login" element={<TeacherLogin/>}/>
+                </Routes>
+            </BrowserRouter>
+            {/* </SemesterProvider> */}
+        </AuthProvider>
+    );
 }
 
 export default App;
